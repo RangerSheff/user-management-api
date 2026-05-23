71b80916ea5c955f060c6ee587758a3a5986511b
Carlos Lopez <carloslopezaguayo@gmail.com>
2026-05-23 18:26:14 -0400

    feat: add database connection postgres and user model

---
+++ app/models/user_model.py
@@ -0,0 +1,62 @@
+import uuid
+
+from sqlalchemy import Boolean
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import String
+from sqlalchemy.sql import func
+
+from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(
+        String,
+        primary_key=True,
+        default=lambda: str(uuid.uuid4())
+    )
+
+    username = Column(
+        String(50),
+        unique=True,
+        nullable=False
+    )
+
+    email = Column(
+        String(255),
+        unique=True,
+        nullable=False
+    )
+
+    first_name = Column(
+        String(100),
+        nullable=False
+    )
+
+    last_name = Column(
+        String(100),
+        nullable=False
+    )
+
+    role = Column(
+        String(20),
+        nullable=False
+    )
+
+    active = Column(
+        Boolean,
+        default=True
+    )
+
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now()
+    )
