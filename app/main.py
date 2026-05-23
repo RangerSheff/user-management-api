1445f8323c15888ed386daae7b2aee74ecf1ea67
Carlos Lopez <carloslopezaguayo@gmail.com>
2026-05-23 18:25:20 -0400

    feat: project structure and configuration basic

---
+++ app/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+
+from app.api.user_router import router as user_router
+from app.core.logging import configure_logging
+from app.core.logging import get_logger
+from app.core.middleware import request_logging_middleware
+from app.db.database import Base
+from app.db.database import engine
+from app.models.user_model import User
+
+configure_logging()
+logger = get_logger(__name__)
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="User Management API",
+    description="FastAPI challenge for Stefanini",
+    version="1.0.0"
+)
+
+app.middleware("http")(request_logging_middleware)
+
+app.include_router(user_router)
+
+
+@app.get("/health")
+def health_check():
+    logger.info("Health check executed")
+    return {
+        "status": "OK"
+    }
